617824651f727794be86308349b1c0fec2fee0ec
zhanjinwin <zhanjin_1988@163.com>
2016-07-29 18:40:09 +0800

    漏提资源文件

--- Volley_downloadfile2/gen/com/example/volley_downloadfile2/R.java
+++ Volley_downloadfile2/gen/com/example/volley_downloadfile2/R.java
@@ -24,9 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080005;
+        public static final int bar=0x7f080004;
+        public static final int fresh_btn=0x7f080002;
+        public static final int pause_btn=0x7f080001;
         public static final int start_btn=0x7f080000;
-        public static final int tips=0x7f080001;
+        public static final int tips=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -37,9 +40,11 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050004;
+        public static final int app_name=0x7f050006;
         public static final int default_txt=0x7f050002;
+        public static final int fresh_btn=0x7f050005;
         public static final int hello_world=0x7f050001;
+        public static final int pause_btn=0x7f050004;
         public static final int start_btn=0x7f050003;
     }
     public static final class style {
